4baac3486c7d0c1ea458d951f777e27e966736aa
Lonne Olmeyer <lonneolmeyer@gmail.com>
2024-09-26 17:34:26 +0200

    functies aangemaakt in database.php #2

--- database.php
+++ database.php
@@ -1,21 +1,41 @@
 <?php
-$SERVER_NAME;
-$DB_NAME;
-$DB_PASS;
-$DB_USERNAME;
+require_once 'config.php';
 
-$conn = mysqli_connect($SERVER_NAME, $DB_USERNAME, $DB_PASS, $DB_NAME);
-
+//connect to database 
+$conn = mysqli_connect(SERVER_NAME, DB_USERNAME, DB_PASS, DB_NAME);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected succesfully";
 
-$sql = "INSERT INTO users (name, email, password)
-VALUES ('Enrique Iglesias', 'e.iglesias@gmail.com', 'pingpongsong!')";
+function getUserByEmail(string $email): array // find user by email adress
+{
+    // prepare statement
+    $search_email = $GLOBALS['conn']->prepare("SELECT FROM users WHERE email=?");
+    $search_email->bind_param("s", $email);
 
-if (mysqli_query($conn, $sql)) {
-    echo "new record created succesfully";
-} else {
-    echo "error: " . $sql . "<br>" . mysqli_error($conn);
+    // set parameters & execute statement 
+    $search_email->execute();
+
+    // get results 
+    $user = $GLOBALS['conn']->query($search_email);
+
+    // close statement
+    $search_email->close();
+
+    // return array
+    return $user;
 }
+
+function createNewUser(string $name, string $email, string $password): void // create new user record 
+{
+    // prepare statement
+    $create_user = $GLOBALS['conn']->prepare("INSERT INTO users (name, email, password) VALUES (?,?,?)");
+    $create_user->bind_param("sss", $name, $email, $password);
+    // set parameters and execute
+    $create_user->execute();
+    // close statement
+    $create_user->close();
+}
+
+// closes opened database connection 
+$conn->close();
