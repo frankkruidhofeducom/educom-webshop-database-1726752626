0958edb7e9f2e77f70808c7ba93b4259109f830c
Lonne Olmeyer <lonneolmeyer@gmail.com>
2024-10-02 13:41:26 +0200

    Shopping cart skeleton #5

--- Pages/shop/cart.php
+++ Pages/shop/cart.php
@@ -1,8 +1,37 @@
 <?php
-function showShoppingCart()
+
+function getCartItems()
+{
+    //get cart items from session manager
+}
+
+
+function showCartItem()
+{
+    // show box with product image, price, quantity etc.
+}
+
+function showShoppingCartStart()
 {
     echo
     '<div class="shoppingcart">
-        <h1>Winkelwagen</h1>
-    </div>';
+        <h1>Winkelwagen</h1>';
+}
+
+function showShoppingCartEnd()
+{   
+    echo 
+    // show cart total 
+    // show button to payment page 
+    '</div>';
+}
+
+function showShoppingCart()
+{   
+    $productsInCart = getCartItems();
+    showShoppingCartStart();
+    foreach ($productsInCart as $cartItem) {
+        showCartItem($cartItem);
+    }
+    showShoppingCartEnd();
 }
